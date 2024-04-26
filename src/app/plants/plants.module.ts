37f902f6adeaa9a2439189addfd948a882bb6faa
Juan Pablo Camacho Caro <jp.camacho10@uniandes.edu.co>
2024-04-25 19:35:42 -0500

    se agrega modulo planta y clase planta

---
+++ src/app/plants/plants.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlantsComponent } from './plants.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [PlantsComponent]
+})
+export class PlantsModule { }
